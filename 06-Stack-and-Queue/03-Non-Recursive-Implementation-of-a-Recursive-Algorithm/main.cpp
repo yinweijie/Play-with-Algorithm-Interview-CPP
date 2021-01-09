fa565b34267f02a9b078a7c2a4d429362e8a7620
yinweijie <ywj123450@gmail.com>
2021-01-09 22:41:08 +0800

    0603 updated

---
+++ 06-Stack-and-Queue/03-Non-Recursive-Implementation-of-a-Recursive-Algorithm/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <cassert>
+
+using namespace std;
+
+/// 144. Binary Tree Preorder Traversal
+/// https://leetcode.com/problems/binary-tree-preorder-traversal/description/
+/// 非递归的二叉树的前序遍历
+/// 时间复杂度: O(n), n为树的节点个数
+/// 空间复杂度: O(h), h为树的高度
