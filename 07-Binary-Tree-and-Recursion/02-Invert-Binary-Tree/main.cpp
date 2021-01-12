093c87d32226b97f98c3fa0a96db84fcf3231c0e
yinweijie <ywj123450@gmail.com>
2021-01-12 22:14:25 +0800

    0702 completed

---
+++ 07-Binary-Tree-and-Recursion/02-Invert-Binary-Tree/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+/// 226. Invert Binary Tree
+/// https://leetcode.com/problems/invert-binary-tree/description/
+/// 时间复杂度: O(n), n为树中节点个数
+/// 空间复杂度: O(h), h为树的高度
+/**
+ * Definition for a binary tree node.
+ */
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(root == nullptr) return nullptr;
+
+        invertTree(root->left);
+        invertTree(root->right);
+        swap(root->left, root->right);
+
+        return root;
+    }
+};
